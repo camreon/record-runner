aee53c4d322aeffe77251e8461739521302126c6
Cameron Guthrie <cameron@thomasstreet.com>
2020-08-03 20:31:09 -0700

    inital commit with basic record running

---
+++ Assets/Scripts/Guy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Guy : MonoBehaviour
+{
+    public Vector3 jump;
+    public float jumpForce = 2.0f;
+    public bool isGrounded;
+    Rigidbody rb;
+
+    void Start(){
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obstacle") {
+            Debug.Log(collision.gameObject);
+
+            rb.AddTorque(transform.up * jumpForce * 10.0f);
+
+            // rb.constraints = RigidbodyConstraints.None;            
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded) {
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Record.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Record : MonoBehaviour
+{
+    public float rotationSpeed = 1.0f;
+    public int spawnRate = 50;
+    public int spawnCounter = 0;
+    public GameObject spawnZone;
+    public GameObject obstacle;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        // rotation mechanics 
+        transform.Rotate(new Vector3(0, rotationSpeed, 0), Space.World);
+   
+        if (Input.GetButtonDown("Fire1")) {
+            rotationSpeed = rotationSpeed * -1.0f;
+        }
+
+        // obstacle mechanics 
+        if (spawnCounter >= spawnRate) {
+            Instantiate(obstacle, spawnZone.transform.position, Quaternion.identity, transform);
+            spawnCounter = 0;
+        }
+
+        spawnCounter++;
+    }
+}
